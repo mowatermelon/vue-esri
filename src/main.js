2fbd3de43c05dd3f7d80484c291958f0b1516283
watermelon <neefoxmo@gmail.com>
2018-03-27 19:58:25 +0800

    添加Vue.use(ElementUI)，即添加ElementUI的全局引用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './assets/js/router'
 import store from './assets/js/vuex/store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 //下面一句配置是去除开发环境下面的多余生产提示
 // You are running Vue in development mode.
@@ -16,6 +20,7 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  render: h => h(App)
 })
 
