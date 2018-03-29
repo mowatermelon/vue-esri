b1bcdeb09aba5723fdb1d8901e1146c1d735b0ff
watermelon <neefoxmo@gmail.com>
2018-03-29 18:44:55 +0800

    优化store中的空格和缩进

--- src/assets/js/vuex/store.js
+++ src/assets/js/vuex/store.js
@@ -1,45 +1,47 @@
+'use strict'
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 import loS from '../../../service/loStorage'
 
-//在使用vuex之前使用，先初始化相关数据
+// 在使用vuex之前使用，先初始化相关数据
 const init = [
-  {name:'isLogin',value:'true'},
-  {name:'themeType',value:'blue'},
-  {name:'userName',value:''},
-  {name:'userPassword',value:''}
-];
+  {name: 'isLogin', value: 'true', type: false},
+  {name: 'themeType', value: 'blue', type: false},
+  {name: 'userName', value: '', type: false},
+  {name: 'userPassword', value: '', type: false}
+]
 
-loS.initData(init);
+loS.initData(init)
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    isLogin: loS.getItem('isLogin',false),
-    themeType: loS.getItem('themeType',true),
-    userName: loS.getItem('userName',true),
-    userPassword: loS.getItem('userPassword',true)
+    isLogin: loS.getItem('isLogin', false),
+    themeType: loS.getItem('themeType', true),
+    userName: loS.getItem('userName', true),
+    userPassword: loS.getItem('userPassword', true)
   },
   mutations: {
     checkLogin (state, code) {
-        localStorage.setItem('isLogin',code);
-        state.isLogin = loS.getItem('isLogin',false);
+      localStorage.setItem('isLogin', code, false)
+      state.isLogin = loS.getItem('isLogin', false)
     },
     setTheme (state, code) {
-      loS.setItem('themeType',code);
-      state.themeType = loS.getItem('themeType',true);
+      loS.setItem('themeType', code, false)
+      state.themeType = loS.getItem('themeType', true)
     },
     setUName (state, code) {
-      loS.setItem('userName',code);
-      state.userName = loS.getItem('userName',true);
+      loS.setItem('userName', code, false)
+      state.userName = loS.getItem('userName', true)
     },
     setUPassd (state, code) {
-      loS.setItem('userPassword',code);
-      state.userPassword = loS.getItem('userPassword',true);
+      loS.setItem('userPassword', code, false)
+      state.userPassword = loS.getItem('userPassword', true)
     }
   }
 
-});
+})
 
 export default store
