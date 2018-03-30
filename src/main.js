c83d14f9049a4f94ca5f9104ef9237e69208ba73
watermelon <neefoxmo@gmail.com>
2018-03-30 13:51:34 +0800

    引入EventBus，将window.EventBus执行指向

--- src/main.js
+++ src/main.js
@@ -4,15 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './assets/js/router'
 import store from './assets/js/vuex/store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import EventBus from './service/Bus'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 
-//下面一句配置是去除开发环境下面的多余生产提示
+// 下面一句配置是去除开发环境下面的多余生产提示
 // You are running Vue in development mode.
 // Make sure to turn on production mode when deploying for production.
 Vue.config.productionTip = false
+
+window.EventBus = EventBus
 
 // 创建和挂载根实例，通过router配置参数注入路由
 new Vue({
