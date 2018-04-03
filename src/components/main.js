9e7292821fa80ba750cb1916eecfdfe793131de0
watermelon <neefoxmo@gmail.com>
2018-04-03 21:04:18 +0800

    一次性添加多个组件功能js，放置后期可能会使用

---
+++ src/components/main.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: Wu Eva
+ * @Date: 2018-04-02 10:48:55
+ * @Last Modified by: Wu Eva
+ * @Last Modified time: 2018-04-03 21:03:32
+ */
+
+'use strict'
+
+import Vue from 'vue'
+
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+const requireComponent = require.context(
+  '.', false, /\.vue$/
+   // 找到components文件夹下以.vue命名的文件
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = capitalizeFirstLetter(
+    fileName.replace(/^\.\//, '').replace(/\.\w+$/, '')
+    // 因为得到的filename格式是: './base.vue', 所以这里我们去掉头和尾，只保留真正的文件名
+  )
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
