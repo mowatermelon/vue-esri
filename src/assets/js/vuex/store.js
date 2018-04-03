c4bf66b0ad725d8a491e2a43e68a015fe131b684
watermelon <neefoxmo@gmail.com>
2018-04-03 21:02:26 +0800

    准备尝试主题换肤功能，全局存储themeColor

--- src/assets/js/vuex/store.js
+++ src/assets/js/vuex/store.js
@@ -7,7 +7,7 @@ import loS from '../../../service/loStorage'
 // 在使用vuex之前使用，先初始化相关数据
 const init = [
   {name: 'isLogin', value: 'true', type: false},
-  {name: 'themeType', value: 'blue', type: false},
+  {name: 'themeColor', value: '#409EFF', type: false},
   {name: 'userName', value: '', type: false},
   {name: 'userPassword', value: '', type: false}
 ]
@@ -19,7 +19,7 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     isLogin: loS.getItem('isLogin', false),
-    themeType: loS.getItem('themeType', true),
+    themeColor: loS.getItem('themeColor', true),
     userName: loS.getItem('userName', true),
     userPassword: loS.getItem('userPassword', true)
   },
@@ -29,8 +29,8 @@ const store = new Vuex.Store({
       state.isLogin = loS.getItem('isLogin', false)
     },
     setTheme (state, code) {
-      loS.setItem('themeType', code, false)
-      state.themeType = loS.getItem('themeType', true)
+      loS.setItem('themeColor', code, false)
+      state.themeColor = loS.getItem('themeColor', true)
     },
     setUName (state, code) {
       loS.setItem('userName', code, false)
