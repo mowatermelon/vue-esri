e82cc3f32c4a1085bb88dae778be3b8818944c4d
watermelon <neefoxmo@gmail.com>
2018-04-04 16:29:12 +0800

    添加比例尺组件

---
+++ src/components/maps/ScaleBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+import esriLoader from 'esri-loader'
+
+export default {
+  name: 'ScaleBar',
+  props: {
+    position:{//
+      type: String,
+      default: "bottom-left"//top-left,top-right,bottom-left,bottom-right
+    }
+  },
+  data () {
+    return {
+      view:{}
+    }
+  },
+  created(){
+    let _this = this;
+    if(!!_this.view){
+      EventBus.$on('setView',function(data){
+        _this.view = data;
+      });
+    }
+    _this.initLoad();
+  },
+  methods: {
+    initLoad: function() {
+      let _this = this;
+      esriLoader.dojoRequire(["esri/widgets/ScaleBar","dojo/domReady!"], (ScaleBar) => {
+        let scaleBar = new ScaleBar({
+          view: _this.view
+        });
+        // Add widget to the bottom left corner of the view
+        _this.view.ui.add(scaleBar, {
+          position: _this.position
+        });
+      });
+      EventBus.$emit('setView',_this.view);
+    }
+  }
+}
+</script>
