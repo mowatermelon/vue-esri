44e55109de197ac47ba7ea21e2dacbfc1a4b1bbc
watermelon <neefoxmo@gmail.com>
2018-04-04 16:42:14 +0800

    添加地图服务相关管理页面

---
+++ src/pages/Manage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <layout></layout>
+  </div>
+</template>
+<script>
+
+import Layout from '../layouts/HeaderAsideFooterResponsiveLayout/Layout'
+
+export default {
+  name: 'Manage',
+  data () {
+    return {
+    }
+  },
+  components:{
+    Layout
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
