47a78af987ed22b96bdeeaa079aea80bbac5b7f7
watermelon <neefoxmo@gmail.com>
2018-04-04 16:41:18 +0800

    添加默认的地图功能页

---
+++ src/pages/Default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <layout></layout>
+  </div>
+</template>
+<script>
+
+import Layout from '../layouts/HeaderFooterResponsiveLayout/Layout'
+
+export default {
+  name: 'Default',
+  data () {
+    return {
+    }
+  },
+  components:{
+    Layout
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
