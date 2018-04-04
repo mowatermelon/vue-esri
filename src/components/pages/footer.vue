2b5e663d59e4f0a09cbe0f8194c3c5fac4977a23
watermelon <neefoxmo@gmail.com>
2018-04-04 16:38:31 +0800

    删除Footer中的多余换行符

--- src/components/pages/footer.vue
+++ src/components/pages/footer.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="mo-oa-footer text-center">
-      <br/>
-      <br/>
-      <br/>
-      <br/>
       Copyright © 2018 cover by WaterMelon <br />鄂ICP备66666666号-5<br />出其东门，有女如云；<br />虽则如云，匪我思存。
   </div>
 </template>
