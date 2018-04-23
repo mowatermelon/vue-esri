a309609a643bb53138705279774fd8834da1d6f7
watermelon <neefoxmo@gmail.com>
2018-04-23 19:43:38 +0800

    添加对于表单input组的组件封装

---
+++ src/components/forms/DialogForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <el-form :model="form">
+      <el-form-item label="活动名称" :label-width="formLabelWidth">
+        <el-input v-model="form.name" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <div class="el-dialog__footer">
+      <el-button @click="emitClose">取 消</el-button>
+      <el-button type="primary" @click="emitClose">确 定</el-button>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'DialogForm',
+  props:{
+    dialogVisible:{
+      type: Boolean,
+      default: false,
+      required: true
+    }
+  },
+  data () {
+    return {
+      form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+      formLabelWidth: '120px',
+      visible: this.dialogVisible
+    }
+  },
+  methods:{
+    emitClose(){
+      this.$emit('increment', false);
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
