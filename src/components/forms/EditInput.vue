05e2a1920cfaa544776f344f78c3f2cf0ef52308
watermelon <neefoxmo@gmail.com>
2018-04-26 19:04:53 +0800

    添加editInput组件

---
+++ src/components/forms/EditInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <span v-if="!edit">{{value}}</span>
+    <el-input placeholder="请输入内容" autocomplete="off" v-model="value" v-if="edit"></el-input>
+    <el-button icon="el-icon-edit" circle @click="edit=true"></el-button>
+    <el-button icon="el-icon-check" circle @click="edit=false"></el-button>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "EditInput",
+  prop:{
+    isEdit:{
+      type: Boolean,
+      default: false,
+      required: true
+    },
+    editValue:{
+      type: String,
+      default: null,
+      required: true
+    }
+  },
+  data() {
+    return {
+      edit:this.isEdit,
+      value:null
+    };
+  },
+  watch:{
+    isEdit: function(){
+      this.edit = this.isEdit
+    },
+    editValue: function(){
+      this.value = this.editValue
+    }
+  },
+  components: {
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
