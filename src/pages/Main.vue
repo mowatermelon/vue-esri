f9853a4ef5bd4a88122bc5aa246d9dd263b48497
watermelon <neefoxmo@gmail.com>
2018-05-22 11:13:15 +0800

    chore:add main components to control secondary routing

---
+++ src/pages/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-container>
+        <el-header>
+            <top-bar></top-bar>
+        </el-header>
+        <el-main class="n-p">
+            <router-view></router-view>
+        </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+const TopBar = resolve => require(['@/components/pages/TopBar'], resolve)
+
+export default {
+  name: "Main",
+  data() {
+    return {};
+  },
+  components:{
+    TopBar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
