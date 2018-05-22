03fcb951f5a94ba2449f8e8f8c9fc7962b33c4a8
watermelon <neefoxmo@gmail.com>
2018-05-22 16:37:25 +0800

    refactor:use loadModules from esri-loader to load esri Module

--- src/components/maps/Views/ScreenView.vue
+++ src/components/maps/Views/ScreenView.vue
@@ -5,99 +5,80 @@
   </div>
 </template>
 <script>
-  import esriLoader from 'esri-loader'
+import { loadModules } from "esri-loader";
 
-  export default {
-    name: 'ScreenView',
-    data () {
-      return {
-        map: {'loaded': ''},
-        isHide: true,
-        evt:{x:'',y:''},
-        camera:{}
-      }
-    },
-    created(){
+export default {
+  name: "ScreenView",
+  data() {
+    return {
+      isHide: true,
+      evt: { x: "", y: "" },
+      camera: {}
+    };
+  },
+  created() {
+    let _this = this;
+    _this.initLoad();
+  },
+  methods: {
+    //初始化
+    initLoad() {
       let _this = this;
-      _this.initLoad();
-    },
-    watch: {
-      'map.loaded': function () {
-        if (this.map.initialized == true) {
-          this.isHide = false;
-        }
-      }
-
-    },
-    methods: {
-      //初始化
-      initLoad(){
-        if (!esriLoader.isLoaded()) {
-          // no, lazy load it the ArcGIS API before using its classes
-          esriLoader.bootstrap((err) => {
-            if (err) {
-              console.error(err);
-            } else {
-              // once it's loaded, create the map
-              this.createMap();
-            }
-          }, {
-            // use a specific version instead of latest 4.x
-            url: '../../../../static/plugins/arcgis46/init.js'
-          });
-        } else {
-          // ArcGIS API is already loaded, just create the map
-          this.createMap();
-        }
-      },
-      // 创建地图
-      createMap: function () {
-        let _this =this;
-
-        esriLoader.dojoRequire(["esri/map", "esri/views/SceneView","dojo/domReady!"], (Map,SceneView) => {
-          _this.map = new Map({
-            basemap: "osm",
-            ground: "world-elevation"// Use the world elevation service
-          });
-          _this.view = new SceneView({
-            container: "viewDiv",     // Reference to the scene div created in step 5
-            map: _this.map,                 // Reference to the map object created before the scene
-            scale: 50000000,          // Sets the initial scale to 1:50,000,000
-            center: [114.40845006666666,30.456864444444443],  // Sets the center point of view with lon/lat
-            camera: {
-              position: [7.654, 45.919, 5184],
-              tilt: 80
-            }
-          });
-          window.view = _this.view;
+      loadModules(["esri/Map", "esri/views/SceneView", "dojo/domReady!"], {
+        url: "../../../../static/plugins/arcgis47/init.js"
+      })
+        .then(([Map, SceneView]) => {
+          _this.createMap(Map, SceneView);
+        })
+        .catch(err => {
+          // handle any errors
+          console.error(err);
         });
-      },
-      // 缩放到中心图层
-      centerZoom: function () {
-        this.map.centerAndZoom([114.40845006666666,30.456864444444443], 16);
-      },
-      // 显示当前坐标
-      showCoordinates: function(e) {
-        let _this = this;
-        _this.view.hitTest(e)
-          .then(function(res){
-            let point = res.results[0].mapPoint;
-            if(!_this.isHide){
-                _this.evt.x = point.x;
-                _this.evt.y = point.y;
-            }
-        });
-      }
-
+    },
+    createMap(Map, SceneView) {
+      let _this = this;
+      let map = new Map({
+        basemap: "osm",
+        ground: "world-elevation" // Use the world elevation service
+      });
+      _this.isHide = false;
+      _this.$store.commit("setMap", map);
+      let view = new SceneView({
+        container: "viewDiv", // Reference to the scene div created in step 5
+        map, // Reference to the map object created before the scene
+        scale: 50000000, // Sets the initial scale to 1:50,000,000
+        center: [114.40845006666666, 30.456864444444443], // Sets the center point of view with lon/lat
+        camera: {
+          position: [7.654, 45.919, 5184],
+          tilt: 80
+        }
+      });
+      _this.$store.commit("setView", view);
+    },
+    // 缩放到中心图层
+    centerZoom: function() {
+      this.map.centerAndZoom([114.40845006666666, 30.456864444444443], 16);
+    },
+    // 显示当前坐标
+    showCoordinates: function(e) {
+      let _this = this;
+      let view = _this.$store.state.view;
+      view.hitTest(e).then(function(res) {
+        let point = res.results[0].mapPoint;
+        if (!_this.isHide) {
+          _this.evt.x = point.x;
+          _this.evt.y = point.y;
+        }
+      });
     }
-
   }
+};
 </script>
 <style lang="scss" scoped>
-@import '../../../../static/plugins/arcgis46/esri/css/main.css';
+@import "../../../../static/plugins/arcgis47/esri/css/main.css";
 .map_box .map_div {
-  width:100%;
-  height: calc(100vh*0.8);
+  width: 100%;
+  height: calc(100vh * 0.8);
   cursor: pointer;
 }
 </style>
