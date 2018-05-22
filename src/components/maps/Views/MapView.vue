0021c3a5db1d8bbcfea952fd629fcf09c92a5673
watermelon <neefoxmo@gmail.com>
2018-05-22 21:26:29 +0800

    perf:add dynamic draw components into the map

--- src/components/maps/Views/MapView.vue
+++ src/components/maps/Views/MapView.vue
@@ -3,7 +3,7 @@
     <div id="viewDiv" class="map_div" @mousemove="showCoordinates($event)" ></div>
     <p class="text-right map-info" :class="{'hide': isHide}">当前坐标：x:{{evt.x}},y:{{evt.y}}</p>
     <scale-bar></scale-bar>
-    <draw drawType="point"></draw>
+    <component :is="drawItem.component" :drawType="drawItem.drawType" v-for="(drawItem,drawIdx) in drawItems" :key="drawIdx"></component>
   </div>
 </template>
 <script>
@@ -14,15 +14,22 @@ import Draw from "../Draw/Draw";
 export default {
   name: "MapView",
   data() {
+    // debugger;
     return {
       isHide: true,
-      evt: { x: "", y: "" }
+      evt: { x: "", y: "" },
+      drawItems: this.$store.state.drawComponents
     };
   },
   created() {
     let _this = this;
     _this.initLoad();
   },
+  // watch: {
+  //   drawItems: function (newItems, oldItems) {
+  //     this.drawItemsCopy = newItems;
+  //   }
+  // },
   methods: {
     //初始化
     initLoad() {
@@ -31,19 +38,19 @@ export default {
       loadModules(["esri/Map", "esri/views/MapView"], {
         url: "../../../../static/plugins/arcgis47/init.js"
       })
-      .then(([Map, MapView]) => {
-        _this.createMap(Map, MapView);
-      })
-      .catch(err => {
-        // handle any errors
-        console.error(err);
-      });
+        .then(([Map, MapView]) => {
+          _this.createMap(Map, MapView);
+        })
+        .catch(err => {
+          // handle any errors
+          console.error(err);
+        });
     },
     createMap(Map, MapView) {
       let _this = this;
       let map = new Map({
         basemap: "osm",
-        showLegend:true
+        showLegend: true
       });
       _this.isHide = false;
       _this.$store.commit("setMap", map);
@@ -58,6 +65,14 @@ export default {
     // 缩放到中心图层
     centerZoom() {
       this.map.centerAndZoom([114.40845006666666, 30.456864444444443], 16);
+    },
+    createDrawing(type) {
+      debugger;
+      let _this = this;
+      _this.drawItems.push({
+        component: "draw",
+        drawType: type
+      });
     },
     // 显示当前坐标
     showCoordinates(e) {
